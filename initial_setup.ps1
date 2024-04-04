a39808e0857d791b82a51e405995dca6827512f4
Jaron Wilding <jaronwilding95@gmail.com>
2024-04-04 15:55:31 +1000

    Update initial_setup.ps1

--- initial_setup.ps1
+++ initial_setup.ps1
@@ -2,7 +2,8 @@
 winget install --silent --id "Git.Git" --accept-source-agreements --accept-package-agreements
 winget install --silent --id "twpayne.chezmoi" --accept-source-agreements --accept-package-agreements
 
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-$env:Path += ";" + [System.Environment]::GetEnvironmentVariable("Path","User")  
+$waitTimeMilliseconds = 1 * 60 * 1000 # Just wait one minute.
 
-chezmoi init --apply jaronwilding
+$powershellPath = "$env:windir\system32\windowspowershell\v1.0\powershell.exe"
+$process = Start-Process $powershellPath -NoNewWindow -ArgumentList ("-ExecutionPolicy Bypass -noninteractive -noprofile chezmoi init --apply jaronwilding") -PassThru
+$process.WaitForExit($waitTimeMilliseconds)
