a6db9233cb5df966b911da13faf8d26e829ca1a7
Jaron Wilding <jaronwilding95@gmail.com>
2024-04-04 15:18:58 +1000

    Update initial_setup.ps1

--- initial_setup.ps1
+++ initial_setup.ps1
@@ -1,3 +1,6 @@
 # Basically just install two programs: Git, and Chezmoi
 winget install --silent --id "Git.Git" --accept-source-agreements --accept-package-agreements
 winget install --silent --id "twpayne.chezmoi" --accept-source-agreements --accept-package-agreements
+
+
+Invoke-Command { & "chezmoi init --apply jaronwilding" } -NoNewScope
