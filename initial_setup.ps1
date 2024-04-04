9feb1890e987c4d42ac6f182b272b607ab15ced1
Jaron Wilding <jaronwilding95@gmail.com>
2024-04-04 16:06:15 +1000

    Update initial_setup.ps1

--- initial_setup.ps1
+++ initial_setup.ps1
@@ -1,9 +1,10 @@
 # Basically just install two programs: Git, and Chezmoi
 winget install --silent --id "Git.Git" --accept-source-agreements --accept-package-agreements
-iex "&{$(irm 'https://get.chezmoi.io/ps1')} -- init --apply https://github.com/jaronwilding/dotfiles"
-# winget install --silent --id "twpayne.chezmoi" --accept-source-agreements --accept-package-agreements
+winget install --silent --id "twpayne.chezmoi" --accept-source-agreements --accept-package-agreements
 
-# $waitTimeMilliseconds = 1 * 60 * 1000 # Just wait one minute.
+$env:Path=(
+    [System.Environment]::GetEnvironmentVariable("Path","Machine"),
+    [System.Environment]::GetEnvironmentVariable("Path","User")
+) -match '.' -join ';'
 
-# $process = Start-Process "powershell.exe" -NoNewWindow -ArgumentList ("-ExecutionPolicy Bypass -noninteractive -noprofile chezmoi init --apply jaronwilding") -PassThru
-# $process.WaitForExit($waitTimeMilliseconds)
+chezmoi init --apply jaronwilding
