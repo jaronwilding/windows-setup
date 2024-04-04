dfbc95c1c4b046a095d8a42ed5b2e6e54b24d7c6
Jaron Wilding <jaronwilding95@gmail.com>
2024-04-04 14:50:13 +1000

    Create initial_setup.ps1

---
+++ initial_setup.ps1
@@ -0,0 +1,3 @@
+# Basically just install two programs: Git, and Chezmoi
+winget install --silent --id "Git.Git" --accept-source-agreements --accept-package-agreements
+winget install --silent --id "twpayne.chezmoi" --accept-source-agreements --accept-package-agreements
