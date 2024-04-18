b016cf8e3148150cba1e9b408d6ff890f782d035
Jaron Wilding <jaronwilding95@gmail.com>
2024-04-18 15:01:12 +1000

    Update setup-windows.ps1

--- windows10/setup-windows.ps1
+++ windows10/setup-windows.ps1
@@ -234,7 +234,8 @@ function Remove-PreinstalledApplications{
     process {
         Write-Verbose "Downloading and using the script from Scynex!"
         $debloater = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/Windows10SysPrepDebloater.ps1" "Windows10SysPrepDebloater.ps1"
-        Invoke-Expression $debloater
+        . $debloater
+        Write-Host "$debloater Sourced"
     }
     end {
         Write-Output "Debloated!"
