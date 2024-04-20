4a0a98ff5a5da2bedbc7019f1ac2ee9e8eacf55d
Jaron Wilding <jaronwilding95@gmail.com>
2024-04-20 20:53:51 +1000

    Following a proper style guide, slowly making changes across multiple files.

--- windows10/installers/install-pythonz.ps1
+++ windows10/installers/install-pythonz.ps1
@@ -1,43 +1,91 @@
 <#
     .SYNOPSIS
-    Installs Python versions
+        Installs Python versions
 
     .DESCRIPTION
-    Installs Python versions to C:\Custom\Managers\Pythonz
-    If already installed, will skip.
+        Installs Python versions to C:\Custom\Managers\Pythonz
+        If already installed, will skip.
 #>
 
-function Get-TempDownloadsFolder{
-    #Variable
-    $parent = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path # Get the downloads folder
-    $download_path = Join-Path $parent "temp"
-    #Check if Folder exists
-    If(!(Test-Path -Path $download_path))
-    {
-        #powershell create directory
-        New-Item -ItemType Directory -Path $download_path
-        Write-Debug "New folder created successfully!"
+function Get-TemporaryDownloadsFolder {
+    <#
+    .SYNOPSIS
+        Creates and returns the temporary folder to download items too.
+
+    .DESCRIPTION
+        Makes a directory of "temp" in the users Downloads folder, and returns the path.
+
+    .INPUTS
+        Accepts the standard arguments, such as -Verbose
+
+    .OUTPUTS
+        Path to the temporary folder located in the Downloads directory.
+
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param()   
+    process {
+        $Parent = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path # Get the downloads folder
+        $TemporaryPath = Join-Path $Parent "temp"
+
+        if ( !(Test-Path -Path $TemporaryPath) ) {
+            Write-Verbose "Creating custom path at: $TemporaryPath"
+            New-Item -Path $TemporaryPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+        }
+
+        return $TemporaryPath
     }
-    return $download_path
 }
 
-function Get-Download{
-    param(
-        [string]$url,
-        [string]$fileName
+function Get-Download {
+    <#
+    .SYNOPSIS
+        Downloads the corresponding url to the given filename.
+    
+    .DESCRIPTION
+        Downloads the file given to the temporary path. Relies on the Get-TemporaryDownloadsFolder cmdlet.
+        It will use curl.exe to start, and fall back to Invoke-WebRequest I.E. wget if that fails.
+
+    .PARAMETER Url
+        The direct-link URL to download from.
+
+    .PARAMETER FileName
+        The name to call the downloaded file.
+
+    .INPUTS
+        Accepts the standard arguments, such as -Verbose
+
+    .OUTPUTS
+        Path to the temporary folder located in the Downloads directory.
+
+    .EXAMPLE
+        Get-Download -Url "https://nim-lang.org/download/nim-2.0.4_x64.zip" -FileName "nim.zip" -Verbose
+        Get-Download "https://nim-lang.org/download/nim-2.0.4_x64.zip" "nim.zip" -Verbose
+        
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Url,
+
+        [Parameter(Mandatory = $true)]
+        [string]$FileName
     )
-    begin {
-        $download_path = Get-TempDownloadsFolder
-        $download_file = Join-Path $download_path $fileName
-    }
     process {
-        If(!(Test-Path -Path $download_file)){
-            Write-Verbose "Downloading $fileName"
-            Invoke-WebRequest "$url" -OutFile $download_file
+        $DownloadPath = Get-TemporaryDownloadsFolder
+        $DownloadFile = Join-Path $DownloadPath $FileName
+        try {
+            Write-Verbose "Downloading $FileName using Curl"
+            Start-Process "curl.exe" -ArgumentList "--no-progress-meter -o $DownloadFile $Url" -Wait -NoNewWindow
         }
-    }
-    end {
-        return $download_file
+        catch {
+            Write-Verbose "Downloading $FileName using standard Powershell cmdlets"
+            Invoke-WebRequest -Uri $Url -OutFile $DownloadFile -Force
+        }
+
+        return $DownloadFile
     }
 }
 
--- windows10/setup-windows.ps1
+++ windows10/setup-windows.ps1
@@ -1,27 +1,38 @@
-# function Expand-Download {
-#     param (
-#         [string]$Folder,
-#         [string]$File
-#     )
-#     if (!(Test-Path -Path "$Folder" -PathType Container)) {
-#         Write-Error "$Folder does not exist!!!"
-#         Break
-#     }
-#     if (Test-Path -Path "$File" -PathType Leaf) {
-#         switch ($File.Split(".") | Select-Object -Last 1) {
-#             "rar" { Start-Process -FilePath "UnRar.exe" -ArgumentList "x","-op'$Folder'","-y","$File" -WorkingDirectory "$Env:ProgramFiles\WinRAR\" -Wait | Out-Null }
-#             "zip" { 7z x -o"$Folder" -y "$File" | Out-Null }
-#             "7z" { 7z x -o"$Folder" -y "$File" | Out-Null }
-#             "exe" { 7z x -o"$Folder" -y "$File" | Out-Null }
-#             Default { Write-Error "No way to Extract $File !!!"; Break }
-#         }
-#     }
-# }
+function Expand-CustomArchive {
+    param (
+        [string]$Folder,
+        [string]$File
+    )
+    if (!(Test-Path -Path "$Folder" -PathType Container)) {
+        Write-Error "$Folder does not exist!!!"
+        Break
+    }
+    $Env:Path = $Env:Path + ";C:\Program Files\7-Zip"
+    if (Test-Path -Path "$File" -PathType Leaf) {
+        switch ($File.Split(".") | Select-Object -Last 1) {
+            "rar" { Start-Process -FilePath "UnRar.exe" -ArgumentList "x","-op'$Folder'","-y","$File" -WorkingDirectory "$Env:ProgramFiles\WinRAR\" -Wait | Out-Null }
+            "zip" { 7z x -o"$Folder" -y "$File" | Out-Null }
+            "7z" { 7z x -o"$Folder" -y "$File" | Out-Null }
+            "exe" { 7z x -o"$Folder" -y "$File" | Out-Null }
+            Default { Write-Error "No way to Extract $File !!!"; Break }
+        }
+    }
+}
 
 # Invoke-WebRequest "https://win.rustup.rs/x86_64"
 
-function Set-Backup{
-    [CmdletBinding()]Param()
+function Backup-Computer{
+    <#
+    .SYNOPSIS
+        Wrapper for two function calls to creating a restore point.
+    .DESCRIPTION
+       Makes use of Enable-ComputerRestore to make sure that creation of a restore point won't error out.
+       Then makes use of Checkpoint-Computer and creates a custom restore point called "RestorePoint1".
+    .INPUTS
+        Accepts the standard arguments, such as -Verbose
+    #>
+    [CmdletBinding()]
+    param()
     begin {
         Write-Output "--------------------------------------------------------"
         Write-Output "Creating a restore point..."
@@ -35,36 +46,85 @@ function Set-Backup{
     }
 }
 
-function Get-TempDownloadsFolder{
-    #Variable
-    $parent = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path # Get the downloads folder
-    $download_path = Join-Path $parent "temp"
-    #Check if Folder exists
-    If(!(Test-Path -Path $download_path))
-    {
-        #powershell create directory
-        New-Item -ItemType Directory -Path $download_path
-        Write-Debug "New folder created successfully!"
-    }
-    Else
-    {
-        Write-Debug "Folder already exists!"
-    }
-    return $download_path
-}
+function Get-TemporaryDownloadsFolder {
+    <#
+    .SYNOPSIS
+        Creates and returns the temporary folder to download items too.
 
-function Get-Download{
-    param(
-        [string]$url,
-        [string]$fileName
-    )
-    $download_path = Get-TempDownloadsFolder
-    $download_file = Join-Path $download_path $fileName
-    If(!(Test-Path -Path $download_file)){
-        Write-Verbose "Downloading $fileName..."
-        Invoke-WebRequest "$url" -OutFile $download_file
+    .DESCRIPTION
+        Makes a directory of "temp" in the users Downloads folder, and returns the path.
+
+    .INPUTS
+        Accepts the standard arguments, such as -Verbose
+
+    .OUTPUTS
+        Path to the temporary folder located in the Downloads directory.
+
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param()   
+    process {
+        $Parent = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path # Get the downloads folder
+        $TemporaryPath = Join-Path $Parent "temp"
+
+        if ( !(Test-Path -Path $TemporaryPath) ) {
+            Write-Verbose "Creating custom path at: $TemporaryPath"
+            New-Item -Path $TemporaryPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+        }
+
+        return $TemporaryPath
     }
-    return $download_file
+}
+function Get-Download {
+    <#
+    .SYNOPSIS
+        Downloads the corresponding url to the given filename.
+    
+    .DESCRIPTION
+        Downloads the file given to the temporary path. Relies on the Get-TemporaryDownloadsFolder cmdlet.
+        It will use curl.exe to start, and fall back to Invoke-WebRequest I.E. wget if that fails.
+
+    .PARAMETER Url
+        The direct-link URL to download from.
+
+    .PARAMETER FileName
+        The name to call the downloaded file.
+
+    .INPUTS
+        Accepts the standard arguments, such as -Verbose
+
+    .OUTPUTS
+        Path to the temporary folder located in the Downloads directory.
+
+    .EXAMPLE
+        Get-Download -Url "https://nim-lang.org/download/nim-2.0.4_x64.zip" -FileName "nim.zip" -Verbose
+        Get-Download "https://nim-lang.org/download/nim-2.0.4_x64.zip" "nim.zip" -Verbose
+
+    #>
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Url,
+
+        [Parameter(Mandatory = $true)]
+        [string]$FileName
+    )
+    process {
+        $DownloadPath = Get-TemporaryDownloadsFolder
+        $DownloadFile = Join-Path $DownloadPath $FileName
+        try {
+            Write-Verbose "Downloading $FileName using Curl"
+            Start-Process "curl.exe" -ArgumentList "--no-progress-meter -o $DownloadFile $Url" -Wait -NoNewWindow
+        }
+        catch {
+            Write-Verbose "Downloading $FileName using standard Powershell cmdlets"
+            Invoke-WebRequest -Uri $Url -OutFile $DownloadFile -Force
+        }
+
+        return $DownloadFile
+    }
 }
 
 function Set-Privacy{
@@ -74,25 +134,11 @@ function Set-Privacy{
         Write-Output "Getting privacy settings."
     }
     process {
-        # $download_path = Get-TempDownloadsFolder
-        # $shutup_10 = Join-Path $download_path "OOSU10.exe"
-        # $shutup_config = Join-Path $download_path "ooshutup10.cfg"
-
-        # If(!(Test-Path -Path $shutup_10)){
-        #     Write-Verbose "Downloading Shutup10..."
-        #     Invoke-WebRequest "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile $shutup_10
-        # }
-
-        # If(!(Test-Path -Path $shutup_config)){
-        #     Write-Verbose "Downloading Shutup10 config file..."
-        #     Invoke-WebRequest "https://github.com/jaronwilding/dotfiles/raw/main/windows10/config/ooshutup10.cfg" -OutFile $shutup_config
-        # }
-
-        $shutup_10 = Get-Download "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" "OOSU10.exe"
-        $shutup_config= Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/config/ooshutup10.cfg" "ooshutup10.cfg"
+        $ShutUp10 = Get-Download -Url "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -FileName "OOSU10.exe"
+        $ShutUpConfig = Get-Download -Url "https://github.com/jaronwilding/dotfiles/raw/main/windows10/config/ooshutup10.cfg" -FileName "ooshutup10.cfg"
 
         Write-Verbose "Running Shutup10 with pre-configured settings..."
-        Start-Process -FilePath $shutup_10 -Verb RunAs -Wait -ArgumentList "$shutup_config", "/quiet"
+        Start-Process -FilePath $ShutUp10 -ArgumentList "$ShutUpConfig /quiet" -Verb RunAs -Wait
     }
     end {
         Write-Output "Privacy settings finished."
@@ -106,16 +152,12 @@ function Optimize-Settings {
         Write-Output "Adjusting settings..."
     }
     process {
-        # $download_path = Get-TempDownloadsFolder
-        # $csvfile = Join-Path $download_path "defaultWindowsSettings.csv"
+        $csvfile = Get-DownloadPath "defaultWindowsSettings.csv"
 
-        # If(!(Test-Path -Path $csvfile)){
-        #     Write-Verbose "Downloading custom settings CSV..."
-            
-        #     Invoke-WebRequest 'https://github.com/jaronwilding/dotfiles/raw/main/windows10/config/defaultWindowsSettings.csv' -OutFile $csvfile
-        # }
-
-        $csvfile = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/config/defaultWindowsSettings.csv" "defaultWindowsSettings.csv"
+        If(!(Test-Path -Path $csvfile)){
+            Write-Verbose "Downloading Default windows settings..."
+            Invoke-WebRequest "https://github.com/jaronwilding/dotfiles/raw/main/windows10/config/defaultWindowsSettings.csv" -OutFile $csvfile
+        }
 
         $settingValues = Import-Csv $csvfile | ForEach-Object {
             [PSCustomObject]@{
@@ -234,21 +276,30 @@ function Remove-PreinstalledApplications{
     process {
         Write-Verbose "Downloading and using the script from Scynex!"
         $debloater = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/Windows10SysPrepDebloater.ps1" "Windows10SysPrepDebloater.ps1"
-        # . $debloater
-        Write-Host "$debloater Sourced"
+        # Start-Process powershell -ArgumentList "$debloater" -Wait
+        # powershell -command - < $debloater
+        # powershell.exe "& $debloater"
+        . $debloater
+
     }
     end {
         Write-Output "Debloated!"
     }
 }
 
-function Install-Applications {
+function Install-ApplicationsWinget {
     [CmdletBinding()]Param()
     begin {
-        Write-Output "--------------------------------------------------------"
-        Write-Output "Installing 3rd-Party tools..."
+        Write-Output "----------------------------"
+        Write-Output "Installing via Winget..."
     }
     process {
+        # Install winget
+        $hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
+        if(!$hasPackageManager){
+            Add-AppxPackage -Path 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle'
+        }
+
         $WinGet = @(
             "Microsoft.DotNet.SDK.3_1",
             "Microsoft.DotNet.SDK.5",
@@ -258,24 +309,131 @@ function Install-Applications {
             "Microsoft.WindowsTerminal",
             "Microsoft.PowerToys",
             "Microsoft.PowerShell",
+            "Starship.Starship",
+            "Docker.DockerDesktop",
+            "chrisant996.Clink",
+            "MediaArea.MediaInfo.GUI",
+            "MediaArea.MediaInfo",
+            "Git.Git",
+            "GitHub.cli",
+            "Piriform.Defraggler",
+            "RARLab.WinRAR",
+            "Mozilla.Firefox",
             "VideoLAN.VLC",
             "Discord.Discord",
             "Valve.Steam",
-            "Starship.Starship",
-            "chrisant996.Clink",
-            "Mozilla.Firefox",
-            "Piriform.Defraggler"
+            "EpicGames.EpicGamesLauncher",
+            "Ubisoft.Connect",
+            "GOG.Galaxy",
+            "ElectronicArts.EADesktop"
+
         )
         ForEach ($item in $WinGet) {
-            Install-WinGetApp -PackageID "$item"
+            Write-Verbose "Installing $item"
+            Install-WinGetApp -PackageID "$item" | Out-Null
         }
     }
     end {
-        Write-Output "Windows 10 has finished setting up."
-        Write-Output "Advised to restart the machine now! :)"
+        Write-Output "Installed all Winget Applications"
     }
 }
 
+function Install-ApplicationsCustom {
+    [CmdletBinding()]Param()
+    begin {
+        Write-Output "----------------------------"
+        Write-Output "Installing via Custom..."
+    }
+    process {
+        $tools_path = "C:\Custom\Tools\"
+        $tweak_path = "C:\Custom\Tweaks\"
+        $temp_path = Get-TemporaryDownloadsFolder
+        if (!(Test-Path -Path $tools_path)) {
+            New-Item $tools_path -ItemType Directory | Out-Null
+        }
+        if (!(Test-Path -Path $tweak_path)) {
+            New-Item $tweak_path -ItemType Directory | Out-Null
+        }
+
+        # -----------------------
+        $zip7 = Get-Download "https://www.7-zip.org/a/7z2404-x64.msi" "7z2404-x64.msi"
+        Write-Verbose "Installing 7zip"
+        Start-Process 'msiexec.exe' -ArgumentList "/I $zip7 /qn" -Wait
+
+        # -----------------------
+        Write-Verbose "Installing FFMPEG"
+        $ffmpeg = Get-Download "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip" "ffmpeg-master-latest-win64-gpl.zip"
+        Expand-Archive $ffmpeg -DestinationPath "$temp_path" -Force
+        Copy-Item -Path "$temp_path\ffmpeg-master-latest-win64-gpl\bin\*.exe" -Destination "$tools_path"
+
+        # -----------------------
+        Write-Verbose "Installing Aria2C"
+        $aria = Get-Download "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0-win-64bit-build1.zip" "aria.zip"
+        Expand-Archive $aria -DestinationPath "$temp_path" -Force
+        Copy-Item -Path "$temp_path\aria2-1.37.0-win-64bit-build1\*.exe" -Destination "$tools_path"
+
+        # -----------------------
+        Write-Verbose "Installing MKVToolNix"
+        $mkvtoolnix = Get-Download "https://mkvtoolnix.download//windows/releases/83.0/mkvtoolnix-64-bit-83.0.7z" "mkvtoolnix-64-bit-83.0.7z"
+        # Extract-Download $ffmpeg -DestinationPath "$temp_path" -Force
+        Extract-Download -Folder "$temp_path" -File "$mkvtoolnix"
+        $mkvtoolnix_items = @(
+            "mkvextract.exe",
+            "mkvinfo.exe",
+            "mkvmerge.exe",
+            "mkvpropedit.exe",
+            "mkvtoolnix-gui.exe"
+        )
+        ForEach($item in $mkvtoolnix_items){
+            Copy-Item -Path "$temp_path\mkvtoolnix\$item" -Destination "$tools_path" -Force
+        }
+        Copy-Item -Path "$temp_path\mkvtoolnix\tools" -Destination "$tools_path" -Force -Recurse
+
+        # -----------------------
+        Write-Verbose "Installing SharkCodecs"
+        $shark_codecs = Get-Download "https://files1.majorgeeks.com/10afebdbffcd4742c81a3cb0f6ce4092156b4375/multimedia/Shark007Codecs.7z" "Shark007Codecs.7z"
+        # Extract-Download $ffmpeg -DestinationPath "$temp_path" -Force
+        Extract-Download -Folder "$tweak_path" -File "$shark_codecs"
+
+    }
+    end {
+        Write-Output "Installed all Custom Applications"
+    }
+}
+
+function Install-Managers {
+    [CmdletBinding()]Param()
+    begin {
+        Write-Output "----------------------------"
+        Write-Output "Installing Managers..."
+    }
+    process {
+        $manager_path = "C:\Custom\Managers"
+        If(!(Test-Path -Path $manager_path))
+        {
+            New-Item -ItemType Directory -Path $manager_path
+        }
+
+        $pyenv = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/installers/install-pythonz.ps1" "install-pythonz.ps1"
+        . "$pyenv"
+        
+        $golang = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/installers/install-golang.ps1" "install-golang.ps1"
+        . "$golang"
+
+        $rustup = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/installers/install-nim.ps1" "install-rustup.ps1"
+        . "$rustup"
+
+        $nim = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/installers/install-nim.ps1" "install-nim.ps1"
+        . "$nim"
+
+        $nvm = Get-Download "https://github.com/jaronwilding/dotfiles/raw/main/windows10/installers/install-nvm.ps1" "install-nvm.ps1"
+        . "$nvm"
+
+    }
+    end {
+        Write-Output "Installed all Managers"
+    }
+}
 
 function Optimize-Windows {
     [CmdletBinding()]Param()
@@ -285,8 +443,10 @@ function Optimize-Windows {
     process {
         # Set-Backup
         Remove-PreinstalledApplications
-        # Install-Application
-        Optimize-Settings
+        # Install-ApplicationsWinget
+        Install-ApplicationsCustom
+        Install-Managers
+        # Optimize-Settings
         # Set-Privacy
         # Enable-WinFeatures
     }
